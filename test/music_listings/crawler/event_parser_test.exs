6db0ac488b77869eb4f455d5203d2cf4a9eed65e
Nick Riebeek <nick.riebeek@gmail.com>
2024-07-12 12:36:19 -0400

    Add some additional testing

---
+++ test/music_listings/crawler/event_parser_test.exs
@@ -0,0 +1,60 @@
+defmodule MusicListings.Crawler.EventParserTest do
+  use MusicListings.DataCase, async: true
+
+  alias MusicListings.Crawler.EventParser
+  alias MusicListings.Crawler.Payload
+  alias MusicListings.Parsing.DanforthMusicHallParser
+  alias MusicListings.PayloadsFixtures
+  alias MusicListingsSchema.Venue
+
+  describe "parse_events/3" do
+    setup do
+      payloads = PayloadsFixtures.load_payloads("test/data/danforth_music_hall/single_event.html")
+      venue = Repo.get_by!(Venue, name: "Danforth Music Hall")
+
+      %{payloads: payloads, venue: venue}
+    end
+
+    test "on successful parse returns payload populated with parsed event", %{
+      payloads: payloads,
+      venue: venue
+    } do
+      [payload] =
+        EventParser.parse_events(payloads, DanforthMusicHallParser, venue)
+
+      venue_id = venue.id
+
+      decimal_30 = Decimal.new("30.00")
+      decimal_50 = Decimal.new("50.00")
+
+      assert %Payload{
+               status: :ok,
+               parsed_event: %MusicListingsSchema.Event{
+                 external_id: "post-17036",
+                 title: "Northlane",
+                 headliner: "Northlane",
+                 openers: ["Invent Animate", "Thornhill", "Windwaker"],
+                 date: ~D[2024-07-05],
+                 time: ~T[18:00:00],
+                 price_format: :range,
+                 price_lo: ^decimal_30,
+                 price_hi: ^decimal_50,
+                 age_restriction: :all_ages,
+                 source_url: "https://thedanforth.com/",
+                 ticket_url: "https://www.ticketmaster.ca/event/1000603D7B880DBA",
+                 venue_id: ^venue_id
+               }
+             } = payload
+    end
+
+    test "on failed parse returns payload populated with parse error", %{venue: venue} do
+      parse_error_payloads =
+        PayloadsFixtures.load_payloads("test/data/danforth_music_hall/parse_error_event.html")
+
+      [payload] =
+        EventParser.parse_events(parse_error_payloads, DanforthMusicHallParser, venue)
+
+      assert :parse_error = payload.status
+    end
+  end
+end
--- test/music_listings/crawler_test.exs
+++ test/music_listings/crawler_test.exs
@@ -1,20 +1,43 @@
 defmodule MusicListings.CrawlerTest do
+  @moduledoc """
+  These are pretty naive tests, might want to enhance them down the road
+  """
   use MusicListings.DataCase, async: true
 
   alias MusicListings.Crawler
+  alias MusicListings.Crawler.CrawlSummary
   alias MusicListings.Parsing.DanforthMusicHallParser
   alias MusicListings.Parsing.VelvetUndergroundParser
   alias MusicListings.Repo
   alias MusicListingsSchema.Event
 
-  # This is a very naive test... we just check the number of
-  # events inserted
   describe "crawl/1" do
     test "expected number of events are inserted" do
       parsers = [DanforthMusicHallParser, VelvetUndergroundParser]
       Crawler.crawl(parsers)
 
       assert 124 = Repo.aggregate(Event, :count)
+    end
+  end
+
+  describe "crawl_summary/1" do
+    setup do
+      parsers = [DanforthMusicHallParser, VelvetUndergroundParser]
+      payloads = Crawler.crawl(parsers)
+
+      %{payloads: payloads}
+    end
+
+    test "summarizes the results of a crawl", %{payloads: payloads} do
+      assert %CrawlSummary{
+               new: 124,
+               updated: 0,
+               duplicate: 0,
+               parse_errors: 0,
+               errors: 0,
+               parse_errors_dump: [],
+               errors_dump: []
+             } == Crawler.crawl_summary(payloads)
     end
   end
 end
