07ffea4b79b1dfe296cb53c7e7bed6d88b95e2ee
Nick Riebeek <nick.riebeek@gmail.com>
2024-07-18 19:34:02 -0400

    Add garrison parser

---
+++ test/music_listings/parsing/garrison_parser_test.exs
@@ -0,0 +1,103 @@
+defmodule MusicListings.Parsing.GarrisonParserTest do
+  use ExUnit.Case, async: true
+
+  alias MusicListings.Parsing.GarrisonParser
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+
+  setup do
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/garrison/index.html")
+
+    single_event_file_path =
+      Path.expand("#{File.cwd!()}/test/data/garrison/single_event.html")
+
+    index_html = File.read!(index_file_path)
+
+    event =
+      single_event_file_path
+      |> File.read!()
+      |> GarrisonParser.event_selector()
+      |> List.first()
+
+    %{index_html: index_html, event: event}
+  end
+
+  describe "source_url/0" do
+    test "returns expected value" do
+      assert "http://www.garrisontoronto.com/listings.html" == GarrisonParser.source_url()
+    end
+  end
+
+  describe "venue_name/0" do
+    test "returns expected value" do
+      assert "The Garrison" == GarrisonParser.venue_name()
+    end
+  end
+
+  describe "event_selector/1" do
+    test "returns expected events", %{index_html: index_html} do
+      events = GarrisonParser.event_selector(index_html)
+
+      assert 32 = Enum.count(events)
+    end
+  end
+
+  describe "next_page_url/1" do
+    test "returns the next page url", %{index_html: index_html} do
+      assert nil == GarrisonParser.next_page_url(index_html)
+    end
+  end
+
+  describe "event_id/1" do
+    test "returns event id", %{event: event} do
+      assert "mizmor_2024-07-19" == GarrisonParser.event_id(event)
+    end
+  end
+
+  describe "event_title/1" do
+    test "returns event title", %{event: event} do
+      assert "MIZMOR" == GarrisonParser.event_title(event)
+    end
+  end
+
+  describe "performers/1" do
+    test "returns the event performers", %{event: event} do
+      assert %Performers{
+               headliner: "MIZMOR",
+               openers: ["AMAROK", "A FLOCK NAMED MURDER", "WHERE THE LIGHT FADES"]
+             } == GarrisonParser.performers(event)
+    end
+  end
+
+  describe "event_date/1" do
+    test "returns the event date", %{event: event} do
+      assert ~D[2024-07-19] == GarrisonParser.event_date(event)
+    end
+  end
+
+  describe "event_time/1" do
+    test "returns the event start time", %{event: event} do
+      assert ~T[19:30:00] == GarrisonParser.event_time(event)
+    end
+  end
+
+  describe "price/1" do
+    test "returns the event price", %{event: event} do
+      assert %Price{format: :fixed, hi: Decimal.new("34.88"), lo: Decimal.new("34.88")} ==
+               GarrisonParser.price(event)
+    end
+  end
+
+  describe "age_restriction/1" do
+    test "returns the event age restriction", %{event: event} do
+      assert :tbd == GarrisonParser.age_restriction(event)
+    end
+  end
+
+  describe "ticket_url/1" do
+    test "returns the event ticket url", %{event: event} do
+      assert "https://inertia-entertainment.com/events/mizmor-amarok/" ==
+               GarrisonParser.ticket_url(event)
+    end
+  end
+end
