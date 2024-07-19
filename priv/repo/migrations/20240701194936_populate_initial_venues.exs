123dea8978e5a13f69299e21b327b44cc456d7fa
Nick Riebeek <nick.riebeek@gmail.com>
2024-07-19 15:56:49 -0400

    Add opera house parser

--- priv/repo/migrations/20240701194936_populate_initial_venues.exs
+++ priv/repo/migrations/20240701194936_populate_initial_venues.exs
@@ -42,7 +42,7 @@ defmodule MusicListings.Repo.Migrations.PopulateInitialVenues do
 
     execute "INSERT INTO venues(name, parser_module_name, pull_events) VALUES('The Monarch Tavern', 'MusicListings.Parsing.MonarchTavernParser', true)"
 
-    execute "INSERT INTO venues(name, parser_module_name, pull_events) VALUES('The Opera House', 'MusicListings.Parsing.OperaHouseParser', false)"
+    execute "INSERT INTO venues(name, parser_module_name, pull_events) VALUES('The Opera House', 'MusicListings.Parsing.OperaHouseParser', true)"
 
     execute "INSERT INTO venues(name, parser_module_name, pull_events) VALUES('The Phoenix', 'MusicListings.Parsing.PhoenixParser', false)"
 
