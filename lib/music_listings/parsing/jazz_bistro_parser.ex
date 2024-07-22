e0bcb21dd0761f6a9092d60d0367047d7c527434
Nick Riebeek <nick.riebeek@gmail.com>
2024-07-22 19:00:46 -0400

    Add jazz bistro parser

---
+++ lib/music_listings/parsing/jazz_bistro_parser.ex
@@ -0,0 +1,88 @@
+defmodule MusicListings.Parsing.JazzBistroParser do
+  @moduledoc """
+  Parser for extracing events from https://jazzbistro.ca/
+  """
+  @behaviour MusicListings.Parsing.Parser
+
+  import Meeseeks.CSS
+
+  alias MusicListings.Parsing.Parser
+  alias MusicListings.Parsing.Performers
+
+  @impl true
+  def source_url, do: "https://jazzbistro.ca/event-calendar/"
+
+  @impl true
+  def venue_name, do: "Jazz Bistro"
+
+  @impl true
+  def example_data_file_location, do: "test/data/jazz_bistro/index.html"
+
+  @impl true
+  def event_selector(body) do
+    body
+    |> Meeseeks.parse()
+    |> Meeseeks.one(css("script[type=\"application/ld+json\"]"))
+    |> Meeseeks.data()
+    |> Jason.decode!()
+    |> Enum.filter(&(&1["@type"] == "Event"))
+  end
+
+  @impl true
+  def next_page_url(_body) do
+    # TODO: think if there is a way to handle specific next pages
+    # as https://jazzbistro.ca/event-calendar/month/2024-08/?mmyy=August%202024
+    # works with this site... the way we currently have it set up it will
+    # only grab the current month
+    # maybe this function takes in an optional current page url?
+
+    # no next page
+    nil
+  end
+
+  @impl true
+  def event_id(event) do
+    # TODO: common
+    slug = "#{event_title(event)}-#{event_date(event)}"
+    Regex.replace(~r/[[:punct:]\s]+/, slug, "_")
+  end
+
+  @impl true
+  def event_title(event) do
+    event["name"]
+    |> String.replace("&#8217;", "'")
+  end
+
+  @impl true
+  def performers(event) do
+    [event_title(event)]
+    |> Performers.new()
+  end
+
+  @impl true
+  def event_date(event) do
+    event["startDate"]
+    |> NaiveDateTime.from_iso8601!()
+    |> NaiveDateTime.to_date()
+  end
+
+  @impl true
+  def event_time(_event) do
+    nil
+  end
+
+  @impl true
+  def price(_event) do
+    Parser.convert_price_string_to_price(nil)
+  end
+
+  @impl true
+  def age_restriction(_event) do
+    :tbd
+  end
+
+  @impl true
+  def ticket_url(event) do
+    event["url"]
+  end
+end
---
+++ test/music_listings/parsing/jazz_bistro_parser_test.exs
@@ -0,0 +1,105 @@
+defmodule MusicListings.Parsing.JazzBistroParserTest do
+  use ExUnit.Case, async: true
+
+  alias MusicListings.Parsing.JazzBistroParser
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+
+  setup do
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/jazz_bistro/index.html")
+
+    single_event_file_path =
+      Path.expand("#{File.cwd!()}/test/data/jazz_bistro/single_event.html")
+
+    index_html = File.read!(index_file_path)
+
+    event =
+      single_event_file_path
+      |> File.read!()
+      |> JazzBistroParser.event_selector()
+      |> List.first()
+
+    %{index_html: index_html, event: event}
+  end
+
+  describe "source_url/0" do
+    test "returns expected value" do
+      assert "https://jazzbistro.ca/event-calendar/" == JazzBistroParser.source_url()
+    end
+  end
+
+  describe "venue_name/0" do
+    test "returns expected value" do
+      assert "Jazz Bistro" == JazzBistroParser.venue_name()
+    end
+  end
+
+  describe "event_selector/1" do
+    test "returns expected events", %{index_html: index_html} do
+      events = JazzBistroParser.event_selector(index_html)
+
+      assert 53 = Enum.count(events)
+    end
+  end
+
+  describe "next_page_url/1" do
+    test "returns the next page url", %{index_html: index_html} do
+      assert nil == JazzBistroParser.next_page_url(index_html)
+    end
+  end
+
+  describe "event_id/1" do
+    test "returns event id", %{event: event} do
+      assert "JB_Piano_Bar_Jim_Clayton_s_Jazz_Jukebox_2024_07_02" ==
+               JazzBistroParser.event_id(event)
+    end
+  end
+
+  describe "event_title/1" do
+    test "returns event title", %{event: event} do
+      assert "JB Piano Bar: Jim Clayton's Jazz Jukebox" ==
+               JazzBistroParser.event_title(event)
+    end
+  end
+
+  describe "performers/1" do
+    test "returns the event performers", %{event: event} do
+      assert %Performers{
+               headliner: "JB Piano Bar: Jim Clayton's Jazz Jukebox",
+               openers: []
+             } == JazzBistroParser.performers(event)
+    end
+  end
+
+  describe "event_date/1" do
+    test "returns the event date", %{event: event} do
+      assert ~D[2024-07-02] == JazzBistroParser.event_date(event)
+    end
+  end
+
+  describe "event_time/1" do
+    test "returns the event start time", %{event: event} do
+      assert nil == JazzBistroParser.event_time(event)
+    end
+  end
+
+  describe "price/1" do
+    test "returns the event price", %{event: event} do
+      assert %Price{format: :tbd, lo: Decimal.new("0"), hi: Decimal.new("0")} ==
+               JazzBistroParser.price(event)
+    end
+  end
+
+  describe "age_restriction/1" do
+    test "returns the event age restriction", %{event: event} do
+      assert :tbd == JazzBistroParser.age_restriction(event)
+    end
+  end
+
+  describe "ticket_url/1" do
+    test "returns the event ticket url", %{event: event} do
+      assert "https://jazzbistro.ca/event/jb-piano-bar-jim-claytons-jazz-jukebox-20/" ==
+               JazzBistroParser.ticket_url(event)
+    end
+  end
+end
