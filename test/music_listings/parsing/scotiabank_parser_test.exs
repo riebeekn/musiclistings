34e61218cb25f9a8c8bcb8ac1b6e8d8fe669b891
Nick Riebeek <nick.riebeek@gmail.com>
2024-07-23 14:42:59 -0400

    Add scotiabank arena parser

---
+++ test/music_listings/parsing/scotiabank_parser_test.exs
@@ -0,0 +1,104 @@
+defmodule MusicListings.Parsing.ScotiabankParserTest do
+  use ExUnit.Case, async: true
+
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+  alias MusicListings.Parsing.ScotiabankParser
+
+  setup do
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/scotiabank/index.html")
+
+    single_event_file_path =
+      Path.expand("#{File.cwd!()}/test/data/scotiabank/single_event.html")
+
+    index_html = File.read!(index_file_path)
+
+    event =
+      single_event_file_path
+      |> File.read!()
+      |> ScotiabankParser.event_selector()
+      |> List.first()
+
+    %{index_html: index_html, event: event}
+  end
+
+  describe "source_url/0" do
+    test "returns expected value" do
+      assert "https://www.scotiabankarena.com/events/events_ajax/9?category=1&venue=0&team=0&exclude=&per_page=63&came_from_page=event-list-page" ==
+               ScotiabankParser.source_url()
+    end
+  end
+
+  describe "venue_name/0" do
+    test "returns expected value" do
+      assert "Scotiabank Arena" == ScotiabankParser.venue_name()
+    end
+  end
+
+  describe "event_selector/1" do
+    test "returns expected events", %{index_html: index_html} do
+      events = ScotiabankParser.event_selector(index_html)
+
+      assert 9 = Enum.count(events)
+    end
+  end
+
+  describe "next_page_url/1" do
+    test "returns the next page url", %{index_html: index_html} do
+      assert nil == ScotiabankParser.next_page_url(index_html)
+    end
+  end
+
+  describe "event_id/1" do
+    test "returns event id", %{event: event} do
+      assert "billy_idol_rebel_yell_2024_08_09" == ScotiabankParser.event_id(event)
+    end
+  end
+
+  describe "event_title/1" do
+    test "returns event title", %{event: event} do
+      assert "Billy Idol: Rebel Yell" == ScotiabankParser.event_title(event)
+    end
+  end
+
+  describe "performers/1" do
+    test "returns the event performers", %{event: event} do
+      assert %Performers{
+               headliner: "Billy Idol: Rebel Yell",
+               openers: []
+             } == ScotiabankParser.performers(event)
+    end
+  end
+
+  describe "event_date/1" do
+    test "returns the event date", %{event: event} do
+      assert ~D[2024-08-09] == ScotiabankParser.event_date(event)
+    end
+  end
+
+  describe "event_time/1" do
+    test "returns the event start time", %{event: event} do
+      assert nil == ScotiabankParser.event_time(event)
+    end
+  end
+
+  describe "price/1" do
+    test "returns the event price", %{event: event} do
+      assert %Price{format: :tbd, lo: Decimal.new("0"), hi: Decimal.new("0")} ==
+               ScotiabankParser.price(event)
+    end
+  end
+
+  describe "age_restriction/1" do
+    test "returns the event age restriction", %{event: event} do
+      assert :tbd == ScotiabankParser.age_restriction(event)
+    end
+  end
+
+  describe "ticket_url/1" do
+    test "returns the event ticket url", %{event: event} do
+      assert "https://www.scotiabankarena.com/events/detail/billy-idol-rebel-yell" ==
+               ScotiabankParser.ticket_url(event)
+    end
+  end
+end
