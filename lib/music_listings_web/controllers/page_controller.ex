c0bcee6dc8d96619eeb0b483b25b3fcb6c1b82ee
Nick Riebeek <nick.riebeek@gmail.com>
2024-08-04 13:36:25 -0400

    Remove default layout and index page

--- lib/music_listings_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule MusicListingsWeb.PageController do
-  use MusicListingsWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
