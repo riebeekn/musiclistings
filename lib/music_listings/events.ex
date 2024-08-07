444dbe95c3b535370495da1ac76458cc33f0d5e2
Nick Riebeek <nick.riebeek@gmail.com>
2024-08-07 15:04:15 -0400

    Refactor into sub context modules

---
+++ lib/music_listings/events.ex
@@ -0,0 +1,39 @@
+defmodule MusicListings.Events do
+  @moduledoc """
+  Context module for event related functionality
+  """
+  import Ecto.Query
+
+  alias MusicListings.Events.PagedEvents
+  alias MusicListings.Repo
+  alias MusicListingsSchema.Event
+  alias MusicListingsUtilities.DateHelpers
+
+  @default_page 1
+  @page_size 10
+
+  @type list_events_opts :: {:page, pos_integer()}
+  @spec list_events(list(list_events_opts)) :: any()
+  def list_events(opts \\ []) do
+    page = opts |> Keyword.get(:page, @default_page) |> max(1)
+
+    today = DateHelpers.now() |> DateHelpers.to_eastern_date()
+
+    pagination_result =
+      Event
+      |> where([event], event.date >= ^today)
+      |> order_by([:date, :title])
+      |> preload(:venue)
+      |> Repo.paginate(page: page, page_size: @page_size)
+
+    grouped_events =
+      pagination_result
+      |> Enum.group_by(& &1.date)
+
+    %PagedEvents{
+      events: grouped_events,
+      current_page: pagination_result.page_number,
+      total_pages: pagination_result.total_pages
+    }
+  end
+end
