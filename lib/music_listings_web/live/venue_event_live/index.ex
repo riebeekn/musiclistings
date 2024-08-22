cca93d270d5afe89ad603e53e7277f619f3eedba
Nick Riebeek <nick.riebeek@gmail.com>
2024-08-22 16:39:12 -0400

    A few more UI updates for the venue events page

--- lib/music_listings_web/live/venue_event_live/index.ex
+++ lib/music_listings_web/live/venue_event_live/index.ex
@@ -44,21 +44,17 @@ defmodule MusicListingsWeb.VenueEventLive.Index do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="mb-12">
+    <div class="mb-6 sm:mb-12">
       <.venue_card venue={@venue} />
     </div>
 
+    <div class="my-4 border-b border-zinc-700" />
     <.page_header header="Upcoming Events" />
+    <div class="mt-4 mb-2 border-b border-zinc-700" />
 
-    <div class="mt-6 overflow-hidden border-t border-zinc-700">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-          <.venue_events_table events={@events} />
-        </div>
-      </div>
-    </div>
+    <.venue_events_table events={@events} />
 
-    <div class="my-8 ml-8">
+    <div class="my-8">
       <.pager
         current_page={@current_page}
         total_pages={@total_pages}
