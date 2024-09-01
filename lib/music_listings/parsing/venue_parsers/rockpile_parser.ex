1deb1586ce3db630ff0fd2c42a86893450b85415
Nick Riebeek <nick.riebeek@gmail.com>
2024-09-01 15:36:18 -0400

    Add rockpile parser

---
+++ lib/music_listings/parsing/venue_parsers/rockpile_parser.ex
@@ -0,0 +1,100 @@
+defmodule MusicListings.Parsing.VenueParsers.RockpileParser do
+  @moduledoc """
+  Parser for extracing events from https://therockpile.ca/
+  """
+  @behaviour MusicListings.Parsing.VenueParser
+
+  import Meeseeks.CSS
+
+  alias MusicListings.Parsing.ParseHelpers
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+  alias MusicListings.Parsing.Selectors
+
+  @impl true
+  def source_url, do: "https://therockpile.ca"
+
+  @impl true
+  def example_data_file_location, do: "test/data/rockpile/index.html"
+
+  @impl true
+  def events(body) do
+    body
+    |> Selectors.all_matches(css(".tw-section"))
+  end
+
+  @impl true
+  def next_page_url(_body, _current_url) do
+    # no next page
+    nil
+  end
+
+  @impl true
+  def event_id(event) do
+    title = event_title(event)
+    date = event_date(event)
+
+    ParseHelpers.build_id_from_title_and_date(title, date)
+  end
+
+  @impl true
+  def ignored_event_id(event) do
+    event_id(event)
+  end
+
+  @impl true
+  def event_title(event) do
+    event
+    |> Selectors.text(css(".tw-name a"))
+  end
+
+  @impl true
+  def performers(event) do
+    [event_title(event)]
+    |> Performers.new()
+  end
+
+  @impl true
+  def event_date(event) do
+    [month_string, day_string, year_string] =
+      event |> Selectors.text(css(".tw-event-date")) |> String.split()
+
+    ParseHelpers.build_date_from_year_month_day_strings(year_string, month_string, day_string)
+  end
+
+  @impl true
+  def additional_dates(_event) do
+    []
+  end
+
+  @impl true
+  def event_time(event) do
+    event
+    |> Selectors.text(css(".tw-event-time"))
+    |> ParseHelpers.time_string_to_time()
+  end
+
+  @impl true
+  def price(_event) do
+    Price.unknown()
+  end
+
+  @impl true
+  def age_restriction(event) do
+    event
+    |> Selectors.text(css(".tw-age-restriction"))
+    |> ParseHelpers.age_restriction_string_to_enum()
+  end
+
+  @impl true
+  def ticket_url(event) do
+    event
+    |> Selectors.url(css(".tw-info-price-buy-tix a"))
+  end
+
+  @impl true
+  def details_url(event) do
+    event
+    |> Selectors.url(css(".tw-name a"))
+  end
+end
---
+++ test/music_listings/parsing/venue_parsers/rockpile_parser_test.exs
@@ -0,0 +1,113 @@
+defmodule MusicListings.Parsing.VenueParsers.RockpileParserTest do
+  use ExUnit.Case, async: true
+
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+  alias MusicListings.Parsing.VenueParsers.RockpileParser
+
+  setup do
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/rockpile/index.html")
+
+    single_event_file_path =
+      Path.expand("#{File.cwd!()}/test/data/rockpile/single_event.html")
+
+    index_html = File.read!(index_file_path)
+
+    event =
+      single_event_file_path
+      |> File.read!()
+      |> RockpileParser.events()
+      |> List.first()
+
+    %{index_html: index_html, event: event}
+  end
+
+  describe "source_url/0" do
+    test "returns expected value" do
+      assert "https://therockpile.ca" == RockpileParser.source_url()
+    end
+  end
+
+  describe "events/1" do
+    test "returns expected events", %{index_html: index_html} do
+      events = RockpileParser.events(index_html)
+
+      assert 44 = Enum.count(events)
+    end
+  end
+
+  describe "next_page_url/2" do
+    test "returns the next page url", %{index_html: index_html} do
+      assert nil == RockpileParser.next_page_url(index_html, nil)
+    end
+  end
+
+  describe "event_id/1" do
+    test "returns event id", %{event: event} do
+      assert "blushing_brides_tribute_to_the_rolling_stones_led_x_zeppelin_led_zeppelin_tribute_2024_10_12" ==
+               RockpileParser.event_id(event)
+    end
+  end
+
+  describe "event_title/1" do
+    test "returns event title", %{event: event} do
+      assert "Blushing Brides / Tribute to The Rolling Stones, Led X Zeppelin - Led Zeppelin Tribute" ==
+               RockpileParser.event_title(event)
+    end
+  end
+
+  describe "performers/1" do
+    test "returns the event performers", %{event: event} do
+      assert %Performers{
+               headliner:
+                 "Blushing Brides / Tribute to The Rolling Stones, Led X Zeppelin - Led Zeppelin Tribute",
+               openers: []
+             } == RockpileParser.performers(event)
+    end
+  end
+
+  describe "event_date/1" do
+    test "returns the event date", %{event: event} do
+      assert ~D[2024-10-12] == RockpileParser.event_date(event)
+    end
+  end
+
+  describe "additional_dates/1" do
+    test "returns a list of additional dates", %{event: event} do
+      assert [] == RockpileParser.additional_dates(event)
+    end
+  end
+
+  describe "event_time/1" do
+    test "returns the event start time", %{event: event} do
+      assert ~T[19:00:00] == RockpileParser.event_time(event)
+    end
+  end
+
+  describe "price/1" do
+    test "returns the event price", %{event: event} do
+      assert %Price{format: :unknown, lo: nil, hi: nil} ==
+               RockpileParser.price(event)
+    end
+  end
+
+  describe "age_restriction/1" do
+    test "returns the event age restriction", %{event: event} do
+      assert :all_ages == RockpileParser.age_restriction(event)
+    end
+  end
+
+  describe "ticket_url/1" do
+    test "returns the event ticket url", %{event: event} do
+      assert "https://www.ticketweb.ca/event/blushing-brides-tribute-to-the-rockpile-bar-nightclub-tickets/13888663?pl=rockpilewest&REFID=clientsitewp" ==
+               RockpileParser.ticket_url(event)
+    end
+  end
+
+  describe "details_url/1" do
+    test "returns the event details url", %{event: event} do
+      assert "https://therockpile.ca/13888663/blushing-brides-tribute-to-the-rolling-stones-led-x-zeppelin-led-zeppelin-tribute" ==
+               RockpileParser.details_url(event)
+    end
+  end
+end
