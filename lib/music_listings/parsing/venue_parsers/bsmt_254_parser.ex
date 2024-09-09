03f1b2fed57326e73d62cda9bbb97c136f036055
Nick Riebeek <nick.riebeek@gmail.com>
2024-09-08 20:10:48 -0400

    Add bsmt 254 parser

---
+++ lib/music_listings/parsing/venue_parsers/bsmt_254_parser.ex
@@ -0,0 +1,90 @@
+defmodule MusicListings.Parsing.VenueParsers.Bsmt254Parser do
+  @moduledoc """
+  Parser for extracing events from https://www.bsmt254.com
+  """
+  @behaviour MusicListings.Parsing.VenueParser
+
+  import Meeseeks.CSS
+
+  alias MusicListings.Parsing.ParseHelpers
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+  alias MusicListings.Parsing.Selectors
+
+  @impl true
+  def source_url, do: "https://www.bsmt254.com/events/"
+
+  @impl true
+  def example_data_file_location, do: "test/data/bsmt_254/index.html"
+
+  @impl true
+  def events(body) do
+    Selectors.all_matches(body, css(".homeEventWrap"))
+  end
+
+  @impl true
+  def next_page_url(_body, _current_url) do
+    nil
+  end
+
+  @impl true
+  def event_id(event) do
+    title = event_title(event)
+    date = event_date(event)
+
+    ParseHelpers.build_id_from_title_and_date(title, date)
+  end
+
+  @impl true
+  def ignored_event_id(event) do
+    event_id(event)
+  end
+
+  @impl true
+  def event_title(event) do
+    Selectors.text(event, css(".eventDetails h3"))
+  end
+
+  @impl true
+  def performers(event) do
+    [event_title(event)]
+    |> Performers.new()
+  end
+
+  @impl true
+  def event_date(event) do
+    [month, day, year, _time, _ampm] = Selectors.text(event, css(".datePull")) |> String.split()
+
+    ParseHelpers.build_date_from_year_month_day_strings(year, month, day)
+  end
+
+  @impl true
+  def additional_dates(_event) do
+    []
+  end
+
+  @impl true
+  def event_time(_event) do
+    nil
+  end
+
+  @impl true
+  def price(_event) do
+    Price.unknown()
+  end
+
+  @impl true
+  def age_restriction(_event) do
+    :unknown
+  end
+
+  @impl true
+  def ticket_url(_event) do
+    nil
+  end
+
+  @impl true
+  def details_url(event) do
+    Selectors.url(event, css(".homeEventWrap"))
+  end
+end
--- priv/repo/migrations/20240908182607_seed_more_venues_8.exs
+++ priv/repo/migrations/20240908182607_seed_more_venues_8.exs
@@ -6,11 +6,20 @@ defmodule MusicListings.Repo.Migrations.SeedMoreVenues8 do
       INSERT INTO venues(name, parser_module_name, pull_events, street, city, province, country, postal_code, google_map_url)
       VALUES('Poetry Jazz Cafe', 'PoetryJazzCafeParser', true, '1078 Queen St W', 'Toronto', 'Ontario', 'Cananda', 'M6J 1H8', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11548.848213554691!2d-79.4217503!3d43.6437563!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34c201443efb%3A0xcf876a6bbce0fc5a!2sPoetry%20Jazz%20Cafe!5e0!3m2!1sen!2sca!4v1725820040627!5m2!1sen!2sca')
     """
+
+    execute """
+      INSERT INTO venues(name, parser_module_name, pull_events, street, city, province, country, postal_code, google_map_url)
+      VALUES('BSMT 254', 'Bsmt254Parser', true, '254 Lansdowne Ave', 'Toronto', 'Ontario', 'Cananda', 'M6H 3X9', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11547.429891630252!2d-79.4403135!3d43.6511336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3527a93a3ea1%3A0x651b92467230216a!2sBSMT254!5e0!3m2!1sen!2sca!4v1725840487607!5m2!1sen!2sca')
+    """
   end
 
   def down do
     execute """
     DELETE FROM venues WHERE name = 'Poetry Jazz Cafe'
+    """
+
+    execute """
+    DELETE FROM venues WHERE name = 'BSMT 254'
     """
   end
 end
