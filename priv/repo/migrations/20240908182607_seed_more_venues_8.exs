125e77630b052710af34368632ac804e293fa8ec
Nick Riebeek <nick.riebeek@gmail.com>
2024-09-08 20:32:12 -0400

    Add castros lounge parser

--- priv/repo/migrations/20240908182607_seed_more_venues_8.exs
+++ priv/repo/migrations/20240908182607_seed_more_venues_8.exs
@@ -11,6 +11,11 @@ defmodule MusicListings.Repo.Migrations.SeedMoreVenues8 do
       INSERT INTO venues(name, parser_module_name, pull_events, street, city, province, country, postal_code, google_map_url)
       VALUES('BSMT 254', 'Bsmt254Parser', true, '254 Lansdowne Ave', 'Toronto', 'Ontario', 'Cananda', 'M6H 3X9', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11547.429891630252!2d-79.4403135!3d43.6511336!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3527a93a3ea1%3A0x651b92467230216a!2sBSMT254!5e0!3m2!1sen!2sca!4v1725840487607!5m2!1sen!2sca')
     """
+
+    execute """
+      INSERT INTO venues(name, parser_module_name, pull_events, street, city, province, country, postal_code, google_map_url)
+      VALUES('Castro''s Lounge', 'CastrosLoungeParser', true, '2116 Queen St E', 'Toronto', 'Ontario', 'Cananda', 'M4E 1E2', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11543.565828222401!2d-79.2951007!3d43.6712272!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cc00c57ad4e3%3A0x94ce1628a0abe719!2sCastro&#39;s%20Lounge!5e0!3m2!1sen!2sca!4v1725841768852!5m2!1sen!2sca')
+    """
   end
 
   def down do
@@ -20,6 +25,10 @@ defmodule MusicListings.Repo.Migrations.SeedMoreVenues8 do
 
     execute """
     DELETE FROM venues WHERE name = 'BSMT 254'
+    """
+
+    execute """
+    DELETE FROM venues WHERE name = 'Castro''s Lounge'
     """
   end
 end
---
+++ test/music_listings/parsing/venue_parsers/castros_lounge_parser_test.exs
@@ -0,0 +1,114 @@
+defmodule MusicListings.Parsing.VenueParsers.CastrosLoungeParserTest do
+  use ExUnit.Case, async: true
+
+  alias MusicListings.Parsing.Performers
+  alias MusicListings.Parsing.Price
+  alias MusicListings.Parsing.VenueParsers.CastrosLoungeParser
+
+  setup do
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/castros_lounge/index.html")
+
+    single_event_file_path =
+      Path.expand("#{File.cwd!()}/test/data/castros_lounge/single_event.html")
+
+    index_html = File.read!(index_file_path)
+
+    event =
+      single_event_file_path
+      |> File.read!()
+      |> CastrosLoungeParser.events()
+
+    %{index_html: index_html, event: event}
+  end
+
+  describe "source_url/0" do
+    test "returns expected value" do
+      assert "https://castroslounge.com/events/month/" == CastrosLoungeParser.source_url()
+    end
+  end
+
+  describe "events/1" do
+    test "returns expected events", %{index_html: index_html} do
+      events = CastrosLoungeParser.events(index_html)
+
+      assert 12 = Enum.count(events)
+    end
+  end
+
+  describe "next_page_url/2" do
+    test "returns the next page url", %{index_html: index_html} do
+      assert nil == CastrosLoungeParser.next_page_url(index_html, nil)
+    end
+  end
+
+  describe "event_id/1" do
+    test "returns event id", %{event: event} do
+      assert "danny_marks_2024_09_28" == CastrosLoungeParser.event_id(event)
+    end
+  end
+
+  describe "ignored_event_id/1" do
+    test "returns ignored event id", %{event: event} do
+      assert "danny_marks_2024_09_28" == CastrosLoungeParser.ignored_event_id(event)
+    end
+  end
+
+  describe "event_title/1" do
+    test "returns event title", %{event: event} do
+      assert "Danny Marks" == CastrosLoungeParser.event_title(event)
+    end
+  end
+
+  describe "performers/1" do
+    test "returns the event performers", %{event: event} do
+      assert %Performers{
+               headliner: "Danny Marks",
+               openers: []
+             } == CastrosLoungeParser.performers(event)
+    end
+  end
+
+  describe "event_date/1" do
+    test "returns the event date", %{event: event} do
+      assert ~D[2024-09-28] == CastrosLoungeParser.event_date(event)
+    end
+  end
+
+  describe "additional_dates/1" do
+    test "returns a list of additional dates", %{event: event} do
+      assert [] == CastrosLoungeParser.additional_dates(event)
+    end
+  end
+
+  describe "event_time/1" do
+    test "returns the event start time", %{event: event} do
+      assert nil == CastrosLoungeParser.event_time(event)
+    end
+  end
+
+  describe "price/1" do
+    test "returns the event price", %{event: event} do
+      assert %Price{format: :unknown, lo: nil, hi: nil} ==
+               CastrosLoungeParser.price(event)
+    end
+  end
+
+  describe "age_restriction/1" do
+    test "returns the event age restriction", %{event: event} do
+      assert :unknown == CastrosLoungeParser.age_restriction(event)
+    end
+  end
+
+  describe "ticket_url/1" do
+    test "returns the event ticket url", %{event: event} do
+      assert nil == CastrosLoungeParser.ticket_url(event)
+    end
+  end
+
+  describe "details_url/1" do
+    test "returns the event details url", %{event: event} do
+      assert "https://castroslounge.com/event/danny-marks-31/" ==
+               CastrosLoungeParser.details_url(event)
+    end
+  end
+end
