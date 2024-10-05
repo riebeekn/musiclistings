0cfef88eca5a0c70a17abfbe4c5412f0d3c49eb2
Nick Riebeek <nick.riebeek@gmail.com>
2024-10-05 15:19:59 -0400

    Change coda external id

--- test/music_listings/parsing/venue_parsers/coda_parser_test.exs
+++ test/music_listings/parsing/venue_parsers/coda_parser_test.exs
@@ -44,13 +44,13 @@ defmodule MusicListings.Parsing.VenueParsers.CodaParserTest do
 
   describe "event_id/1" do
     test "returns event id", %{event: event} do
-      assert "lee_foss_2024_07_13" == CodaParser.event_id(event)
+      assert "coda_2024_07_13" == CodaParser.event_id(event)
     end
   end
 
   describe "ignored_event_id/1" do
     test "returns ignored event id", %{event: event} do
-      assert "lee_foss_2024_07_13" == CodaParser.ignored_event_id(event)
+      assert "coda_2024_07_13" == CodaParser.ignored_event_id(event)
     end
   end
 
