52074c6ffc8d6efa45f41872ff4469b4d444f8ce
Nick Riebeek <nick.riebeek@gmail.com>
2024-11-09 15:43:25 -0500

    Add phx gen auth

--- mix.exs
+++ mix.exs
@@ -50,6 +50,7 @@ defmodule MusicListings.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
