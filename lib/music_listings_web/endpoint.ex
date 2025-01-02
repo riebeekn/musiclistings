7ddd2166d74c2f9b07209ccc80f2605d3f45537d
Nick Riebeek <nick.riebeek@gmail.com>
2025-01-02 16:45:44 -0500

    Add health check plug

--- lib/music_listings_web/endpoint.ex
+++ lib/music_listings_web/endpoint.ex
@@ -1,6 +1,8 @@
 defmodule MusicListingsWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :music_listings
 
+  plug MusicListingsWeb.Plugs.HealthCheck
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
