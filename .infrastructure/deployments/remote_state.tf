27d9921210f4403f6f3872a0e3ba1bc356501ecd
Nick Riebeek <nick.riebeek@gmail.com>
2025-01-04 14:55:36 -0500

    Move tf state storage to S3

--- .infrastructure/deployments/remote_state.tf
+++ .infrastructure/deployments/remote_state.tf
@@ -2,9 +2,10 @@
 # Reference to the core remote state
 
 data "terraform_remote_state" "core" {
-  backend = "local"
-
+  backend = "s3"
   config = {
-    path = "../core/terraform.tfstate"
+    bucket = "terraform-musiclistings-state"
+    key    = "core/terraform.tfstate"
+    region = "us-east-1"
   }
 }
