4c7f0df435879625c40f6d349d2b0335398dedad
Nick Riebeek <nick.riebeek@gmail.com>
2025-03-11 20:59:36 -0400

    Update great canadian casino  to LN parser

--- lib/music_listings/parsing/venue_parsers/great_canadian_casino_parser.ex
+++ lib/music_listings/parsing/venue_parsers/great_canadian_casino_parser.ex
@@ -4,48 +4,28 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParser do
   """
   @behaviour MusicListings.Parsing.VenueParser
 
-  import Meeseeks.CSS
-
-  alias MusicListings.HttpClient
-  alias MusicListings.Parsing.ParseHelpers
-  alias MusicListings.Parsing.Performers
-  alias MusicListings.Parsing.Price
-  alias MusicListings.Parsing.Selectors
+  alias MusicListings.Parsing.VenueParsers.BaseParsers.LiveNationParser
 
   @impl true
-  def source_url,
-    do:
-      "https://api.greatcanadian.com/wp-json/snap_widgets_api/v1/entertainment?lang=en&property=23676"
+  defdelegate source_url, to: LiveNationParser
 
+  @live_nation_venue_id "KovZ917Acbk"
   @impl true
   def retrieve_events_fun do
-    fn url -> HttpClient.get(url) end
+    LiveNationParser.retrieve_events_fun(@live_nation_venue_id)
   end
 
   @impl true
   def example_data_file_location, do: "test/data/great_canadian_casino/index.json"
 
   @impl true
-  def events(body) do
-    body = ParseHelpers.maybe_decode!(body)
-
-    body["renderd_content"]
-    |> ParseHelpers.clean_html()
-    |> Selectors.all_matches(css(".promo-card"))
-  end
+  defdelegate events(body), to: LiveNationParser
 
   @impl true
-  def next_page_url(_body, _current_url) do
-    nil
-  end
+  defdelegate next_page_url(body, current_url), to: LiveNationParser
 
   @impl true
-  def event_id(event) do
-    title = event_title(event)
-    date = event_date(event)
-
-    ParseHelpers.build_id_from_title_and_date(title, date)
-  end
+  defdelegate event_id(event), to: LiveNationParser
 
   @impl true
   def ignored_event_id(event) do
@@ -53,107 +33,29 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParser do
   end
 
   @impl true
-  def event_title(event) do
-    Selectors.text(event, css(".promo-content h2"))
-  end
+  defdelegate event_title(event), to: LiveNationParser
 
   @impl true
-  def performers(event) do
-    [event_title(event)]
-    |> Performers.new()
-  end
-
-  @doc """
-  Dates unfortunately take a number of different formats:
-  - September 19, 2024 - September 22, 2024
-  - July 15 & 16, 2025"
-  - Saturday, October 19, 2024
-  """
-  @impl true
-  def event_date(event) do
-    date_string = Selectors.text(event, css(".promo-date.promo-widget-home .row .col"))
-
-    if String.contains?(date_string, "-") do
-      [start_date_string, _end_date_string] = String.split(date_string, "-")
-      parse_date_string(start_date_string)
-    else
-      parse_date_string(date_string)
-    end
-  end
-
-  @regex ~r/(?<month>\w+)\s+(?<day1>\d{1,2})(?:\s*&\s*(?<day2>\d{1,2}))?,\s+(?<year>\d{4})/
-  defp parse_date_string(date_string, opts \\ []) do
-    for_day = Keyword.get(opts, :for_day, :day1)
-
-    %{
-      "month" => month_string,
-      "day1" => day1_string,
-      "day2" => day2_string,
-      "year" => year_string
-    } = Regex.named_captures(@regex, date_string)
-
-    if for_day == :day1 do
-      ParseHelpers.build_date_from_year_month_day_strings(year_string, month_string, day1_string)
-    else
-      if day2_string == "" do
-        nil
-      else
-        ParseHelpers.build_date_from_year_month_day_strings(
-          year_string,
-          month_string,
-          day2_string
-        )
-      end
-    end
-  end
+  defdelegate performers(event), to: LiveNationParser
 
   @impl true
-  def additional_dates(event) do
-    date_string = Selectors.text(event, css(".promo-date.promo-widget-home .row .col"))
-
-    if String.contains?(date_string, "-") do
-      [start_date_string, end_date_string] = String.split(date_string, "-")
-      start_date = parse_date_string(start_date_string)
-      end_date = parse_date_string(end_date_string)
-
-      [_opening_date | remaining_dates] =
-        Date.range(start_date, end_date) |> Enum.to_list()
-
-      remaining_dates
-    else
-      additional_date = parse_date_string(date_string, for_day: :day2)
-
-      if additional_date do
-        [additional_date]
-      else
-        []
-      end
-    end
-  end
+  defdelegate event_date(event), to: LiveNationParser
 
   @impl true
-  def event_time(_event) do
-    nil
-  end
+  defdelegate additional_dates(event), to: LiveNationParser
 
   @impl true
-  def price(_event) do
-    Price.unknown()
-  end
+  defdelegate event_time(event), to: LiveNationParser
 
   @impl true
-  def age_restriction(_event) do
-    :unknown
-  end
+  defdelegate price(event), to: LiveNationParser
 
   @impl true
-  def ticket_url(event) do
-    Selectors.url(event, css(".btn.gold"))
-  end
+  defdelegate age_restriction(event), to: LiveNationParser
 
   @impl true
-  def details_url(event) do
-    slug = Selectors.url(event, css(".promo-img a"))
-    "https://greatcanadian.com#{slug}"
-  end
+  defdelegate ticket_url(event), to: LiveNationParser
+
+  @impl true
+  defdelegate details_url(event), to: LiveNationParser
 end
--- test/music_listings/parsing/venue_parsers/great_canadian_casino_parser_test.exs
+++ test/music_listings/parsing/venue_parsers/great_canadian_casino_parser_test.exs
@@ -16,15 +16,14 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParserTest do
     event =
       single_event_file_path
       |> File.read!()
-      |> GreatCanadianCasinoParser.events()
-      |> List.first()
+      |> Jason.decode!()
 
     %{index_html: index_html, event: event}
   end
 
   describe "source_url/0" do
     test "returns expected value" do
-      assert "https://api.greatcanadian.com/wp-json/snap_widgets_api/v1/entertainment?lang=en&property=23676" ==
+      assert "https://api.livenation.com/graphql" ==
                GreatCanadianCasinoParser.source_url()
     end
   end
@@ -33,7 +32,7 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParserTest do
     test "returns expected events", %{index_html: index_html} do
       events = GreatCanadianCasinoParser.events(index_html)
 
-      assert 19 == Enum.count(events)
+      assert 12 == Enum.count(events)
     end
   end
 
@@ -45,21 +44,21 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParserTest do
 
   describe "event_id/1" do
     test "returns event id", %{event: event} do
-      assert "frank_turner_presents_lost_evenings_vii_2024_09_19" ==
+      assert "1AvZZb3GkUg-3mX" ==
                GreatCanadianCasinoParser.event_id(event)
     end
   end
 
   describe "ignored_event_id/1" do
     test "returns ignored event id", %{event: event} do
-      assert "frank_turner_presents_lost_evenings_vii_2024_09_19" ==
+      assert "1AvZZb3GkUg-3mX" ==
                GreatCanadianCasinoParser.ignored_event_id(event)
     end
   end
 
   describe "event_title/1" do
     test "returns event title", %{event: event} do
-      assert "Frank Turner Presents Lost Evenings VII" ==
+      assert "Trivium & Bullet for My Valentine: The Poisoned Ascendancy" ==
                GreatCanadianCasinoParser.event_title(event)
     end
   end
@@ -67,28 +66,27 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParserTest do
   describe "performers/1" do
     test "returns the event performers", %{event: event} do
       assert %Performers{
-               headliner: "Frank Turner Presents Lost Evenings VII",
-               openers: []
+               headliner: "Trivium",
+               openers: ["Bullet for My Valentine", "August Burns Red", "Bleed From Within"]
              } == GreatCanadianCasinoParser.performers(event)
     end
   end
 
   describe "event_date/1" do
     test "returns the event date", %{event: event} do
-      assert ~D[2024-09-19] == GreatCanadianCasinoParser.event_date(event)
+      assert ~D[2025-05-04] == GreatCanadianCasinoParser.event_date(event)
     end
   end
 
   describe "additional_dates/1" do
     test "returns a list of additional dates", %{event: event} do
-      assert [~D[2024-09-20], ~D[2024-09-21], ~D[2024-09-22]] ==
-               GreatCanadianCasinoParser.additional_dates(event)
+      assert [] == GreatCanadianCasinoParser.additional_dates(event)
     end
   end
 
   describe "event_time/1" do
     test "returns the event start time", %{event: event} do
-      assert nil == GreatCanadianCasinoParser.event_time(event)
+      assert ~T[18:30:00] == GreatCanadianCasinoParser.event_time(event)
     end
   end
 
@@ -107,14 +105,14 @@ defmodule MusicListings.Parsing.VenueParsers.GreatCanadianCasinoParserTest do
 
   describe "ticket_url/1" do
     test "returns the event ticket url", %{event: event} do
-      assert "https://www.ticketmaster.ca/the-theatre-at-great-canadian-casino-tickets-toronto/venue/132544" ==
+      assert "https://www.ticketmaster.ca/trivium-bullet-for-my-valentine-the-toronto-ontario-05-04-2025/event/1000612DB5852BBE" ==
                GreatCanadianCasinoParser.ticket_url(event)
     end
   end
 
   describe "details_url/1" do
     test "returns the event details url", %{event: event} do
-      assert "https://greatcanadian.com/event/lost-evenings/" ==
+      assert nil ==
                GreatCanadianCasinoParser.details_url(event)
     end
   end
