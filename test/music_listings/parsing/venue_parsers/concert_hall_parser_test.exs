dee7a2657abbd3e7a10aca078bef424a5edb84ce
Nick Riebeek <nick.riebeek@gmail.com>
2025-07-05 12:51:26 -0400

    Update concert hall parser

--- test/music_listings/parsing/venue_parsers/concert_hall_parser_test.exs
+++ test/music_listings/parsing/venue_parsers/concert_hall_parser_test.exs
@@ -32,7 +32,7 @@ defmodule MusicListings.Parsing.VenueParsers.ConcertHallParserTest do
     test "returns expected events", %{index_html: index_html} do
       events = ConcertHallParser.events(index_html)
 
-      assert 37 = Enum.count(events)
+      assert 27 = Enum.count(events)
     end
   end
 
@@ -44,26 +44,26 @@ defmodule MusicListings.Parsing.VenueParsers.ConcertHallParserTest do
 
   describe "event_id/1" do
     test "returns event id", %{event: event} do
-      assert "concert_hall_2024_08_31_19_00_00" == ConcertHallParser.event_id(event)
+      assert "concert_hall_2025_08_12_07_00_00" == ConcertHallParser.event_id(event)
     end
   end
 
   describe "ignored_event_id/1" do
     test "returns ignored event id", %{event: event} do
-      assert "concert_hall_2024_08_31_19_00_00" == ConcertHallParser.ignored_event_id(event)
+      assert "concert_hall_2025_08_12_07_00_00" == ConcertHallParser.ignored_event_id(event)
     end
   end
 
   describe "event_title/1" do
     test "returns event title", %{event: event} do
-      assert "Crumb" == ConcertHallParser.event_title(event)
+      assert "Watchhouse" == ConcertHallParser.event_title(event)
     end
   end
 
   describe "performers/1" do
     test "returns the event performers", %{event: event} do
       assert %Performers{
-               headliner: "Crumb",
+               headliner: "Watchhouse",
                openers: []
              } == ConcertHallParser.performers(event)
     end
@@ -71,7 +71,7 @@ defmodule MusicListings.Parsing.VenueParsers.ConcertHallParserTest do
 
   describe "event_date/1" do
     test "returns the event date", %{event: event} do
-      assert ~D[2024-08-31] == ConcertHallParser.event_date(event)
+      assert ~D[2025-08-12] == ConcertHallParser.event_date(event)
     end
   end
 
@@ -83,13 +83,13 @@ defmodule MusicListings.Parsing.VenueParsers.ConcertHallParserTest do
 
   describe "event_time/1" do
     test "returns the event start time", %{event: event} do
-      assert ~T[19:00:00] == ConcertHallParser.event_time(event)
+      assert ~T[07:00:00] == ConcertHallParser.event_time(event)
     end
   end
 
   describe "price/1" do
     test "returns the event price", %{event: event} do
-      assert %Price{format: :unknown, lo: nil, hi: nil} ==
+      assert %Price{format: :fixed, lo: Decimal.new("49.32"), hi: Decimal.new("49.32")} ==
                ConcertHallParser.price(event)
     end
   end
@@ -102,8 +102,7 @@ defmodule MusicListings.Parsing.VenueParsers.ConcertHallParserTest do
 
   describe "ticket_url/1" do
     test "returns the event ticket url", %{event: event} do
-      assert "https:\/\/www.ticketweb.ca\/event\/crumb-with-special-guests-the-concert-hall-tickets\/13407644?pl=MODO-LIVE" ==
-               ConcertHallParser.ticket_url(event)
+      assert "https://link.dice.fm/H6d610248a48" == ConcertHallParser.ticket_url(event)
     end
   end
 
