037eafb88ca2309fc1f8de507e49c6cd53593870
Nick Riebeek <nick.riebeek@gmail.com>
2026-01-11 13:20:56 -0500

    Update dprtmnt parser

--- test/music_listings/parsing/venue_parsers/dprtmnt_parser_test.exs
+++ test/music_listings/parsing/venue_parsers/dprtmnt_parser_test.exs
@@ -6,25 +6,18 @@ defmodule MusicListings.Parsing.VenueParsers.DprtmntParserTest do
   alias MusicListings.Parsing.VenueParsers.DprtmntParser
 
   setup do
-    index_file_path = Path.expand("#{File.cwd!()}/test/data/dprtmnt/index.json")
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/dprtmnt/index.html")
+    single_event_file_path = Path.expand("#{File.cwd!()}/test/data/dprtmnt/single_event.html")
 
-    single_event_file_path =
-      Path.expand("#{File.cwd!()}/test/data/dprtmnt/single_event.json")
-
-    index_html = index_file_path |> File.read!() |> Jason.decode!()
-
-    event =
-      single_event_file_path
-      |> File.read!()
-      |> Jason.decode!()
+    index_html = File.read!(index_file_path)
+    event = File.read!(single_event_file_path)
 
     %{index_html: index_html, event: event}
   end
 
   describe "source_url/0" do
     test "returns expected value" do
-      assert "https://core.service.elfsight.com/p/boot/?page=https%3A%2F%2Fdprtmnt.com%2Fevents%2F&w=a969d3fe-6f22-4cb5-a7f1-1b03dad77e15" ==
-               DprtmntParser.source_url()
+      assert "https://dprtmnt.com/events/" == DprtmntParser.source_url()
     end
   end
 
@@ -32,7 +25,7 @@ defmodule MusicListings.Parsing.VenueParsers.DprtmntParserTest do
     test "returns expected events", %{index_html: index_html} do
       events = DprtmntParser.events(index_html)
 
-      assert 134 = Enum.count(events)
+      assert 11 = Enum.count(events)
     end
   end
 
@@ -44,26 +37,31 @@ defmodule MusicListings.Parsing.VenueParsers.DprtmntParserTest do
 
   describe "event_id/1" do
     test "returns event id", %{event: event} do
-      assert "m66qqgz5" == DprtmntParser.event_id(event)
+      [event] = DprtmntParser.events(event)
+      assert "4270" == DprtmntParser.event_id(event)
     end
   end
 
   describe "ignored_event_id/1" do
     test "returns ignored event id", %{event: event} do
-      assert "m66qqgz5" == DprtmntParser.ignored_event_id(event)
+      [event] = DprtmntParser.events(event)
+      assert "4270" == DprtmntParser.ignored_event_id(event)
     end
   end
 
   describe "event_title/1" do
     test "returns event title", %{event: event} do
-      assert "ROBIN SCHULZ" == DprtmntParser.event_title(event)
+      [event] = DprtmntParser.events(event)
+      assert "CHUS & CEBALLOS" == DprtmntParser.event_title(event)
     end
   end
 
   describe "performers/1" do
     test "returns the event performers", %{event: event} do
+      [event] = DprtmntParser.events(event)
+
       assert %Performers{
-               headliner: "ROBIN SCHULZ",
+               headliner: "CHUS & CEBALLOS",
                openers: []
              } == DprtmntParser.performers(event)
     end
@@ -71,24 +69,29 @@ defmodule MusicListings.Parsing.VenueParsers.DprtmntParserTest do
 
   describe "event_date/1" do
     test "returns the event date", %{event: event} do
-      assert ~D[2025-03-07] == DprtmntParser.event_date(event)
+      [event] = DprtmntParser.events(event)
+      assert ~D[2025-01-16] == DprtmntParser.event_date(event)
     end
   end
 
   describe "additional_dates/1" do
     test "returns a list of additional dates", %{event: event} do
+      [event] = DprtmntParser.events(event)
       assert [] == DprtmntParser.additional_dates(event)
     end
   end
 
   describe "event_time/1" do
     test "returns the event start time", %{event: event} do
-      assert ~T[23:45:00] == DprtmntParser.event_time(event)
+      [event] = DprtmntParser.events(event)
+      assert nil == DprtmntParser.event_time(event)
     end
   end
 
   describe "price/1" do
     test "returns the event price", %{event: event} do
+      [event] = DprtmntParser.events(event)
+
       assert %Price{format: :unknown, hi: nil, lo: nil} ==
                DprtmntParser.price(event)
     end
@@ -96,20 +99,24 @@ defmodule MusicListings.Parsing.VenueParsers.DprtmntParserTest do
 
   describe "age_restriction/1" do
     test "returns the event age restriction", %{event: event} do
-      assert :unknown == DprtmntParser.age_restriction(event)
+      [event] = DprtmntParser.events(event)
+      assert :nineteen_plus == DprtmntParser.age_restriction(event)
     end
   end
 
   describe "ticket_url/1" do
     test "returns the event ticket url", %{event: event} do
-      assert nil == DprtmntParser.ticket_url(event)
+      [event] = DprtmntParser.events(event)
+
+      assert "https://www.ticketweb.ca/event/chus-ceballos-dprtmnt-tickets/13986904?pl=ink" ==
+               DprtmntParser.ticket_url(event)
     end
   end
 
   describe "details_url/1" do
     test "returns the event details url", %{event: event} do
-      assert "https://dprtmnt.com/robin-schulz/" ==
-               DprtmntParser.details_url(event)
+      [event] = DprtmntParser.events(event)
+      assert "https://dprtmnt.com/events/" == DprtmntParser.details_url(event)
     end
   end
 end
