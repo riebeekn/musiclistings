40df039fe1575504d6a8c8543ff55e31fb15d98f
Nick Riebeek <nick.riebeek@gmail.com>
2026-01-11 20:53:08 -0500

    Update rebel parser

--- test/music_listings/parsing/venue_parsers/rebel_parser_test.exs
+++ test/music_listings/parsing/venue_parsers/rebel_parser_test.exs
@@ -6,25 +6,18 @@ defmodule MusicListings.Parsing.VenueParsers.RebelParserTest do
   alias MusicListings.Parsing.VenueParsers.RebelParser
 
   setup do
-    index_file_path = Path.expand("#{File.cwd!()}/test/data/rebel/index.json")
+    index_file_path = Path.expand("#{File.cwd!()}/test/data/rebel/index.html")
+    single_event_file_path = Path.expand("#{File.cwd!()}/test/data/rebel/single_event.html")
 
-    single_event_file_path =
-      Path.expand("#{File.cwd!()}/test/data/rebel/single_event.json")
-
-    index_html = index_file_path |> File.read!() |> Jason.decode!()
-
-    event =
-      single_event_file_path
-      |> File.read!()
-      |> Jason.decode!()
+    index_html = File.read!(index_file_path)
+    event = File.read!(single_event_file_path)
 
     %{index_html: index_html, event: event}
   end
 
   describe "source_url/0" do
     test "returns expected value" do
-      assert "https://core.service.elfsight.com/p/boot/?page=https%3A%2F%2Frebeltoronto.com%2Fevents%2F&w=737e2434-3a70-460f-aa98-a1ec67d0b60b" ==
-               RebelParser.source_url()
+      assert "https://rebeltoronto.com/events/" == RebelParser.source_url()
     end
   end
 
@@ -32,7 +25,7 @@ defmodule MusicListings.Parsing.VenueParsers.RebelParserTest do
     test "returns expected events", %{index_html: index_html} do
       events = RebelParser.events(index_html)
 
-      assert 327 = Enum.count(events)
+      assert 20 = Enum.count(events)
     end
   end
 
@@ -44,26 +37,31 @@ defmodule MusicListings.Parsing.VenueParsers.RebelParserTest do
 
   describe "event_id/1" do
     test "returns event id", %{event: event} do
-      assert "lw7xanxp" == RebelParser.event_id(event)
+      [event] = RebelParser.events(event)
+      assert "7487" == RebelParser.event_id(event)
     end
   end
 
   describe "ignored_event_id/1" do
     test "returns ignored event id", %{event: event} do
-      assert "lw7xanxp" == RebelParser.ignored_event_id(event)
+      [event] = RebelParser.events(event)
+      assert "7487" == RebelParser.ignored_event_id(event)
     end
   end
 
   describe "event_title/1" do
     test "returns event title", %{event: event} do
-      assert "FRENCH MONTANA - GOTTA SEE IT TO BELIEVE IT TOUR" == RebelParser.event_title(event)
+      [event] = RebelParser.events(event)
+      assert "SUBTRONICS" == RebelParser.event_title(event)
     end
   end
 
   describe "performers/1" do
     test "returns the event performers", %{event: event} do
+      [event] = RebelParser.events(event)
+
       assert %Performers{
-               headliner: "FRENCH MONTANA - GOTTA SEE IT TO BELIEVE IT TOUR",
+               headliner: "SUBTRONICS",
                openers: []
              } == RebelParser.performers(event)
     end
@@ -71,24 +69,30 @@ defmodule MusicListings.Parsing.VenueParsers.RebelParserTest do
 
   describe "event_date/1" do
     test "returns the event date", %{event: event} do
-      assert ~D[2024-08-15] == RebelParser.event_date(event)
+      [event] = RebelParser.events(event)
+      # Mock date is 2024-08-01, so January 23 is calculated as next year
+      assert ~D[2025-01-23] == RebelParser.event_date(event)
     end
   end
 
   describe "additional_dates/1" do
     test "returns a list of additional dates", %{event: event} do
+      [event] = RebelParser.events(event)
       assert [] == RebelParser.additional_dates(event)
     end
   end
 
   describe "event_time/1" do
     test "returns the event start time", %{event: event} do
-      assert ~T[19:00:00] == RebelParser.event_time(event)
+      [event] = RebelParser.events(event)
+      assert nil == RebelParser.event_time(event)
     end
   end
 
   describe "price/1" do
     test "returns the event price", %{event: event} do
+      [event] = RebelParser.events(event)
+
       assert %Price{format: :unknown, hi: nil, lo: nil} ==
                RebelParser.price(event)
     end
@@ -96,20 +100,24 @@ defmodule MusicListings.Parsing.VenueParsers.RebelParserTest do
 
   describe "age_restriction/1" do
     test "returns the event age restriction", %{event: event} do
-      assert :unknown == RebelParser.age_restriction(event)
+      [event] = RebelParser.events(event)
+      assert :nineteen_plus == RebelParser.age_restriction(event)
     end
   end
 
   describe "ticket_url/1" do
     test "returns the event ticket url", %{event: event} do
-      assert nil == RebelParser.ticket_url(event)
+      [event] = RebelParser.events(event)
+
+      assert "https://www.ticketweb.ca/event/subtronics-fibonacci-tour-rebel-tickets/14610773?pl=ink" ==
+               RebelParser.ticket_url(event)
     end
   end
 
   describe "details_url/1" do
     test "returns the event details url", %{event: event} do
-      assert "https://rebeltoronto.com/2024/05/15/french-montana-gotta-see-it-to-believe-it-tour/" ==
-               RebelParser.details_url(event)
+      [event] = RebelParser.events(event)
+      assert "https://rebeltoronto.com/events/" == RebelParser.details_url(event)
     end
   end
 end
