815ba11e9689e38c8c1862f19e6f8478a7a4e7c7
Nick Riebeek <nick.riebeek@gmail.com>
2026-01-17 10:27:34 -0500

    Fix dialyzer and compile warnings

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,3 +1,6 @@
 [
-  {"lib/music_listings.ex", :invalid_contract}
+  {"lib/music_listings.ex", :invalid_contract},
+  # Ecto.Multi uses MapSet internally, which is opaque in newer Elixir/OTP versions
+  # These are false positives - the code is correct
+  {"lib/music_listings/accounts.ex", :call_without_opaque}
 ]
