252e75c87607cf56ec4a83721650cdea4405ac9b
Nick Riebeek <nick.riebeek@gmail.com>
2026-04-04 13:18:01 -0400

    Add retry logic

--- lib/music_listings/http_client/req.ex
+++ lib/music_listings/http_client/req.ex
@@ -6,10 +6,18 @@ defmodule MusicListings.HttpClient.Req do
 
   alias MusicListings.HttpClient.Response
 
+  @retry_statuses [403, 408, 429, 500, 502, 503, 504]
+
   @impl true
   def get(url, headers \\ []) do
     url
-    |> Req.get(headers: headers, finch: MusicListings.ReqFinch, receive_timeout: 30_000)
+    |> Req.get(
+      headers: headers,
+      finch: MusicListings.ReqFinch,
+      receive_timeout: 30_000,
+      retry: &retry?/2,
+      max_retries: 3
+    )
     |> case do
       {:ok, response} -> {:ok, Response.new(response)}
       {:error, error} -> {:error, error}
@@ -23,11 +31,17 @@ defmodule MusicListings.HttpClient.Req do
       headers: headers,
       json: body,
       finch: MusicListings.ReqFinch,
-      receive_timeout: 30_000
+      receive_timeout: 30_000,
+      retry: &retry?/2,
+      max_retries: 3
     )
     |> case do
       {:ok, response} -> {:ok, Response.new(response)}
       {:error, error} -> {:error, error}
     end
   end
+
+  defp retry?(_request, %{status: status}) when status in @retry_statuses, do: true
+  defp retry?(_request, %Req.TransportError{}), do: true
+  defp retry?(_request, _response), do: false
 end
