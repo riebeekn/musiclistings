b09c3ebdcf5db3dc285fba4d9e962c3b6fb36ab0
Nick Riebeek <nick.riebeek@gmail.com>
2026-05-03 12:27:58 -0400

    Decode entities in feed

--- lib/music_listings_web/controllers/feed_controller.ex
+++ lib/music_listings_web/controllers/feed_controller.ex
@@ -22,9 +22,9 @@ defmodule MusicListingsWeb.FeedController do
     <?xml version="1.0" encoding="UTF-8"?>
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
-        <title>#{escape(SEO.site_name())}</title>
+        <title>#{plain_text_escape(SEO.site_name())}</title>
         <link>#{escape(channel_link)}</link>
-        <description>#{escape(SEO.default_description())}</description>
+        <description><![CDATA[#{html_escape(SEO.default_description())}]]></description>
         <language>en-ca</language>
         <lastBuildDate>#{build_date}</lastBuildDate>
         <atom:link rel="self" type="application/rss+xml" href="#{escape(self_link)}" />
@@ -36,12 +36,13 @@ defmodule MusicListingsWeb.FeedController do
 
   defp render_item(event) do
     url = SEO.event_url(event)
-    description = SEO.event_meta_description(event)
+    title = event.title |> decode_entities() |> plain_text_escape()
+    description = event |> SEO.event_meta_description() |> decode_entities() |> html_escape()
     pub_date = rfc822(event.updated_at)
 
     """
         <item>
-          <title>#{escape(event.title)}</title>
+          <title>#{title}</title>
           <link>#{escape(url)}</link>
           <guid isPermaLink="true">#{escape(url)}</guid>
           <pubDate>#{pub_date}</pubDate>
@@ -64,4 +65,53 @@ defmodule MusicListingsWeb.FeedController do
     |> String.replace("\"", "&quot;")
     |> String.replace("'", "&apos;")
   end
+
+  # RSS titles are plain text. Hex character references render correctly in
+  # both XML-decoding and HTML-decoding readers, avoiding ambiguity with named
+  # HTML entities.
+  defp plain_text_escape(value) when is_binary(value) do
+    value
+    |> String.replace("&", "&#x26;")
+    |> String.replace("<", "&#x3C;")
+    |> String.replace(">", "&#x3E;")
+  end
+
+  # RSS descriptions are commonly parsed as HTML. Pre-escaping `&`/`<`/`>` and
+  # wrapping in CDATA means the content survives both the XML and HTML decode
+  # passes that feed readers perform.
+  defp html_escape(value) when is_binary(value) do
+    value
+    |> String.replace("&", "&amp;")
+    |> String.replace("<", "&lt;")
+    |> String.replace(">", "&gt;")
+  end
+
+  defp decode_entities(value) when is_binary(value) do
+    value
+    |> decode_numeric_entities()
+    |> decode_named_entities()
+  end
+
+  defp decode_numeric_entities(value) do
+    value
+    |> then(&Regex.replace(~r/&#(\d+);/, &1, fn _, num -> codepoint(String.to_integer(num)) end))
+    |> then(
+      &Regex.replace(~r/&#x([0-9a-fA-F]+);/, &1, fn _, hex ->
+        codepoint(String.to_integer(hex, 16))
+      end)
+    )
+  end
+
+  defp decode_named_entities(value) do
+    value
+    |> String.replace("&lt;", "<")
+    |> String.replace("&gt;", ">")
+    |> String.replace("&quot;", "\"")
+    |> String.replace("&apos;", "'")
+    |> String.replace("&nbsp;", " ")
+    |> String.replace("&amp;", "&")
+  end
+
+  defp codepoint(n) when n in 0..0x10FFFF, do: <<n::utf8>>
+  defp codepoint(_), do: ""
 end
