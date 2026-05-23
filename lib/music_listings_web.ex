bd1cab4a3f3d91d8cc3da0ba1029d1b807cf0009
Nick Riebeek <nick.riebeek@gmail.com>
2026-05-23 14:28:40 -0400

    Update favicon

--- lib/music_listings_web.ex
+++ lib/music_listings_web.ex
@@ -17,7 +17,8 @@ defmodule MusicListingsWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt llms.txt)
+  def static_paths,
+    do: ~w(assets fonts images favicon.ico favicon.svg apple-touch-icon.png robots.txt llms.txt)
 
   def router do
     quote do
